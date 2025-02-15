ecf596838d851328dc149142d54f51d447210293
Sara Ines Gonzalez <107520803+sarainesgonz@users.noreply.github.com>
2025-02-14 22:22:27 -0300

    creates provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@heroui/react";
+import {  FaBeer, FaCoffee } from "react-icons/fa";
+
 export default function Home() {
   return (
    <div>
     <h1 className="text-3xl font-bold">Tirame un centro!</h1>
+    <Button color="danger" variant="flat" startContent={<FaCoffee />}>
+      Charlita?
+    </Button>
    </div>
   );
 }
---
+++ src/components/Providers.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+import {HeroUIProvider} from '@heroui/react'
+
+export default function Providers({children} : {children: React.ReactNode}) {
+  return (
+    <HeroUIProvider>
+        {children}
+    </HeroUIProvider>
+)
+}
+// pass this to app
