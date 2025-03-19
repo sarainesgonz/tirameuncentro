3aac0af4088c66c6ad69e46f21e6dd89ca88d039
Sara Ines Gonzalez <107520803+sarainesgonz@users.noreply.github.com>
2025-03-19 15:31:01 -0300

    adds context to manage session data

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,16 +1,27 @@
 'use client'
 
-import React from 'react'
+import React, {createContext,  useContext } from 'react';
 import {HeroUIProvider} from '@heroui/react'
 // make toast available everywhere
 import { ToastContainer } from 'react-toastify'
+
 // import 'react-toastify/dist/ReactToastify.css'  
 
-export default function Providers({children} : {children: React.ReactNode}) {
+const SessionContext = createContext<any>(null); //creo context para la session
+
+// exporto hook para usar la session en cualquier componente
+export function useSession() {
+  return useContext(SessionContext)
+}
+
+export default function Providers({children, session} : {children: React.ReactNode, session: any}) {
   return (
     <HeroUIProvider>
+      <SessionContext.Provider value={session}>
+
       <ToastContainer position="bottom-center"/> 
         {children}
+      </SessionContext.Provider>
     </HeroUIProvider>
 )
 }
